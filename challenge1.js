9c186a7c29a793b43c97e02137d60b78ab94cbc3
paul kairu <paulkairukamau@gmail.com>
2024-10-31 18:09:57 +0300

    add challenge1.js

---
+++ challenge1.js
@@ -0,0 +1,15 @@
+var React = require("react");
+
+function createElement(content = "Hello World", tag = "div", props = {}) {
+  return React.createElement(tag, props, content);
+}
+function createUnorderedList(list) { 
+  // create a react unordered list with children list items created form the list argument
+  return React.createElement(
+    "ul",
+    {},
+    list.map((x,index)=>
+             React.createElement("li",{key:x-${index}},x)
+            )
+  );
+}
